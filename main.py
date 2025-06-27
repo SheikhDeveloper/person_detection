d2dec8320e5bdf1c583937164585c76d9910f158
Sheikh <tvemperror00@gmail.com>
2025-06-27 17:53:06 +0300

    Change pop-up error for invalid amount of arguments

--- main.py
+++ main.py
@@ -3,6 +3,6 @@ import sys
 
 if __name__ == '__main__':
     if len(sys.argv) != 3 or sys.argv[1][-4:] != '.mp4' or sys.argv[2][-4:] != '.mp4':
-        print('Usage: python main.py <input_video_name>.mp4 <output_video_name>.mp4', file=sys.stderr, flush=True)
+        print('Usage: python main.py <path_to_input_video> <path_to_output_video>', file=sys.stderr, flush=True)
         sys.exit(1)
     process_video(sys.argv[1], sys.argv[2])
